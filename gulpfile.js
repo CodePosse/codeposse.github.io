4acf66bc07e840ad2ecb7d204f0585936192b2ae
Tim Hunold <hunold@google.com>
2019-04-17 11:34:41 -0700

    Gulp and optimizations

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var gulp = require("gulp");
+var cheerio = require("gulp-cheerio"); //use jquery in gulp
+var gutil = require("gulp-util"); //utilities
+var plumber = require('gulp-plumber'); //error handler
+var sass = require("gulp-sass"); //scss/sass task
+var uglify = require("gulp-uglify");
+var uglifycss = require("gulp-uglifycss");
+var htmlmin = require("gulp-htmlmin"); //html uglify
+
+gulp.task("default", async function() {
+  gutil.log(
+    gutil.colors.bgGreen.white.bold("GULP WORKS"),
+    gutil.colors.bgRed.white.bold('type: "gulp --tasks" to list all tasks')
+  );
+});
+gulp.task("miniHTML", async function() {
+  gulp
+    .src("src/html/*.html") //DUDE, CHANGE ME
+    .pipe(plumber())
+    .pipe(
+      cheerio(function($, file) {
+        $("img:not([alt])").attr("alt", ""); //this adds a blank alt tag to images without an alt
+      })
+    )
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+        removeComments: true,
+        minifyCSS: true,
+        minifyJS: true
+      })
+    )
+    .pipe(gulp.dest("./prod/html/")); //DUDE, CHANGE ME
+});
+gulp.task("sass", async function() {
+  gulp
+    .src("./src/scss/theme.scss") //DUDE, CHANGE ME
+    .pipe(sass())
+    .pipe(uglifycss())
+    .pipe(gulp.dest("src/css")); //DUDE, CHANGE ME
+});
+gulp.task("miniJS", async function() {
+  gulp
+    .src("./src/js/*.js") //DUDE, CHANGE ME
+    .pipe(uglify())
+    .pipe(gulp.dest("./js")); //DUDE, CHANGE ME
+});
+gulp.task("miniCSS", async function() {
+  gulp
+    .src("./src/css/*.css") //DUDE, CHANGE ME
+    .pipe(uglifycss())
+    .pipe(gulp.dest("./css")); //DUDE, CHANGE ME
+});
---
+++ src/js/myscript.js
@@ -0,0 +1,79 @@
+$(document).ready(function(){	
+//this is just silly stuff if you want to look at my source, and to mess with crawlers
+	var title = $(this).attr('title');
+	var myUrl = "www.ItsSoBig.com";
+	var myName = "Tim Hunold"
+	a = { Name: "Tim Hunold", Location: "LA", Experience: "18 years", Phone: "310.929.7155", Site_Personal: "ItsSoBig.com", Site_Professional: "linkedin.com/in/itssobig", Email: title + '@gmail.com', Specialties: "HTML, CSS, jQuery, JavaScript, Grunt, SASS, CMS" };
+	console.info('Yeah, I got a GitHub and this is a stupid dev trick. If you saw this in console, congrats. If you saw this in view-source, bite me. ' + 'Now look at the object.');
+	console.dir(a);
+	$(".eml").append("<a href='mailto:" + title + "@gmail.com?subject=Contact from GitHub&body=Say Something'>" + title + "@gmail.com</a>");
+	$(".myUrl").append("<a href='http://" + myUrl + "/'>" + myUrl + "</a>");
+	$(".myName").append(myName);
+	
+	//Menu respond
+	if ($(window).width()<480){
+		$(".menu_btn a").click(function () {
+			$(".mainmenu").slideToggle("slow");
+		}); 	
+	};
+	
+	
+	//Contact form
+	$("#ajax-contact-form").submit(function() {
+		var str = $(this).serialize();		
+		$.ajax({
+			type: "POST",
+			url: "contact_form/contact_process.php",
+			data: str,
+			success: function(msg) {
+				// Message Sent - Show the 'Thank You' message and hide the form
+				if(msg == 'OK') {
+					result = '<div class="notification_ok">Your message has been sent. Thank you!</div>';
+					$("#fields").hide();
+				} else {
+					result = msg;
+				}
+				$('#note').html(result);
+			}
+		});
+		return false;
+	});
+	
+	
+	//Image hover
+	$(".hover_img").mouseover(function(){
+			var info=$(this).find("img");
+			info.stop().animate({opacity:0.15},500);
+		}
+	);
+	$(".hover_img").mouseout(function(){
+			var info=$(this).find("img");
+			info.stop().animate({opacity:1},500);
+		}
+	);
+
+
+});
+
+
+$(window).load(function(){
+	vertAlign();
+	
+});
+
+
+$(window).resize(function(){
+	vertAlign();
+});
+
+
+
+/*function vertAlign(){
+	if ($(window).width()>979){
+		var body_h = $(window).height();
+		var container_h = $('.wrap').height();	
+		var padding_top = Math.abs((body_h - container_h)/2);	
+		$('.wrap').css('padding-top', padding_top);
+		$('.wrap').css('padding-bottom', padding_top);
+	}
+}*/
