c171546f9e668a878a834e624cc6cf6197a5a041
Tim Hunold <hunold@google.com>
2019-04-17 11:43:20 -0700

    Added SourceMaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 var gulp = require("gulp");
 var cheerio = require("gulp-cheerio"); //use jquery in gulp
 var gutil = require("gulp-util"); //utilities
-var plumber = require('gulp-plumber'); //error handler
+var htmlmin = require("gulp-htmlmin"); //html uglify
+var plumber = require("gulp-plumber"); //error handler
 var sass = require("gulp-sass"); //scss/sass task
+var sourcemaps = require("gulp-sourcemaps");
 var uglify = require("gulp-uglify");
 var uglifycss = require("gulp-uglifycss");
-var htmlmin = require("gulp-htmlmin"); //html uglify
 
 gulp.task("default", async function() {
   gutil.log(
@@ -13,6 +14,7 @@ gulp.task("default", async function() {
     gutil.colors.bgRed.white.bold('type: "gulp --tasks" to list all tasks')
   );
 });
+
 gulp.task("miniHTML", async function() {
   gulp
     .src("src/html/*.html") //DUDE, CHANGE ME
@@ -32,22 +34,31 @@ gulp.task("miniHTML", async function() {
     )
     .pipe(gulp.dest("./prod/html/")); //DUDE, CHANGE ME
 });
+
 gulp.task("sass", async function() {
   gulp
     .src("./src/scss/theme.scss") //DUDE, CHANGE ME
+    .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(uglifycss())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest("src/css")); //DUDE, CHANGE ME
 });
+
 gulp.task("miniJS", async function() {
   gulp
     .src("./src/js/*.js") //DUDE, CHANGE ME
+    .pipe(sourcemaps.init())
     .pipe(uglify())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest("./js")); //DUDE, CHANGE ME
 });
+
 gulp.task("miniCSS", async function() {
   gulp
     .src("./src/css/*.css") //DUDE, CHANGE ME
+    .pipe(sourcemaps.init())
     .pipe(uglifycss())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest("./css")); //DUDE, CHANGE ME
 });
