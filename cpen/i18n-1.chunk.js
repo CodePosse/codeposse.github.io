b39b535b9877311aa4499bfe9d7afb3430af9045
T. Hunold <codeposse@gmail.com>
2019-10-08 10:11:50 -0700

    cpen test

---
+++ cpen/i18n-1.chunk.js
@@ -0,0 +1,30 @@
+webpackJsonp([2],{
+
+/***/ "/MHg":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* eslint-disable *//* harmony default export */ __webpack_exports__["default"] = ({
+	languageData: {
+		plurals: function (n, ord) {
+			if (ord) return 'other';
+			return n == 1 ? 'one' : 'other';
+		}
+	},
+	messages: {
+		'Add a JS/CSS library': 'Add a JS/CSS library',
+		'Add library': 'Add library',
+		Console: 'Console',
+		Login: 'Login',
+		New: 'New',
+		Open: 'Open',
+		Run: 'Run',
+		Save: 'Save',
+		Signup: 'Signup'
+	}
+});
+
+/***/ })
+
+});
