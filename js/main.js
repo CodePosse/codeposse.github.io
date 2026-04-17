44cbd4bdc4a0871d72cfc0398035d62723a7ab24
T.Hunold <codeposse@gmail.com>
2026-04-17 09:45:18 -0700

    :wrench: speed

--- js/main.js
+++ js/main.js
@@ -247,62 +247,6 @@
                 }
             });
 
-        // Lightbox init
-        $('body').magnificPopup({
-            delegate: 'a.lightbox',
-            type: 'image',
-            removalDelay: 300,
-
-            // Class that is added to popup wrapper and background
-            // make it unique to apply your CSS animations just to this exact popup
-            mainClass: 'mfp-fade',
-            image: {
-                // options for image content type
-                titleSrc: 'title',
-                gallery: {
-                    enabled: true
-                },
-            },
-
-            iframe: {
-                markup: '<div class="mfp-iframe-scaler">' +
-                    '<div class="mfp-close"></div>' +
-                    '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
-                    '<div class="mfp-title mfp-bottom-iframe-title"></div>' +
-                    '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
-
-                patterns: {
-                    youtube: {
-                        index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
-
-                        id: null, // String that splits URL in a two parts, second part should be %id%
-                        // Or null - full URL will be returned
-                        // Or a function that should return %id%, for example:
-                        // id: function(url) { return 'parsed id'; }
-
-                        src: '%id%?autoplay=1' // URL that will be set as a source for iframe.
-                    },
-                    vimeo: {
-                        index: 'vimeo.com/',
-                        id: '/',
-                        src: '//player.vimeo.com/video/%id%?autoplay=1'
-                    },
-                    gmaps: {
-                        index: '//maps.google.',
-                        src: '%id%&output=embed'
-                    }
-                },
-
-                srcAction: 'iframe_src', // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
-            },
-
-            callbacks: {
-                markupParse: function (template, values, item) {
-                    values.title = item.el.attr('title');
-                }
-            },
-        });
-
         //Google Maps
         if ($(".lmpixels-map")[0]) {
             var address = '10082 Westwanda Drive, Beverly Hills', //Replace with Your Address
